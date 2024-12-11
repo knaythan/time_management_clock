4cd03e9e10150c7525d5e4b04f55c92634f40892
Sshahparnia <shervan.shahparnia@gmail.com>
2024-12-10 20:52:12 -0800

    main adjusted to include mac support

--- clock/main.py
+++ clock/main.py
@@ -3,6 +3,7 @@ from gui import SmartClockApp
 import customtkinter as ctk
 from PIL import Image, ImageTk, ImageDraw  # Import ImageDraw
 import os
+import platform
 
 def main():
     ctk.set_appearance_mode("System")  # Modes: "System" (default), "Dark", "Light"
@@ -22,11 +23,14 @@ def main():
     # Convert the PIL image to a PhotoImage for tkinter
     icon_photo = ImageTk.PhotoImage(image)
     # Save the icon image as a .ico file
-    icon_path = os.path.join(os.path.dirname("../icons"), "icon.ico")
-    image.save(icon_path, format='ICO')
+    if platform.system() == "Darwin":
+        root.iconphoto(True, icon_photo)  # Set the custom icon
+    else:
+        icon_path = os.path.join(os.path.dirname("../icons"), "icon.ico")
+        image.save(icon_path, format='ICO')
 
-    # Set the custom icon
-    root.iconbitmap(icon_path)  # Set the custom icon
+        # Set the custom icon
+        root.iconbitmap(icon_path)  # Set the custom icon
 
     app = SmartClockApp(root)
     app.run()
