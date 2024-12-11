bf7d2e774da29d14ad6d5762d793f12c26072d86
Sshahparnia <shervan.shahparnia@gmail.com>
2024-12-11 06:09:37 -0800

    settings

--- clock/settings.py
+++ clock/settings.py
@@ -1,21 +1,13 @@
 import json
 import os
 import sqlite3
+import customtkinter
 
 class Settings:
     def __init__(self):
         self.file_path = "settings.json"
-        self.db_path = os.path.join(os.path.dirname(__file__), '../db/usage_data.db')
-        self.data = {
-            "custom_app_names": {},
-            "autosave": True,  # Default to autosave enabled
-            "pomodoro_timer": 25,  # Default Pomodoro timer in minutes
-            "short_break": 5,  # Default short break in minutes
-            "long_break": 15,  # Default long break in minutes
-            "long_break_interval": 4  # Number of Pomodoros before a long break
-        }
+        self.data = {}
         self.load()
-        self._setup_database()
 
     def load(self):
         """Load settings from a file if it exists."""
@@ -32,24 +24,8 @@ class Settings:
         """Update a setting and save it."""
         self.data[key] = value
         self.save()
-
+        
     def get(self, key):
         """Retrieve a setting value."""
         return self.data.get(key, None)
-
-    def _setup_database(self):
-        """Set up SQLite database for saving focus times."""
-        os.makedirs(os.path.dirname(self.db_path), exist_ok=True)
-        conn = sqlite3.connect(self.db_path)
-        cursor = conn.cursor()
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS usage_data (
-                id INTEGER PRIMARY KEY,
-                date TEXT NOT NULL,
-                app_name TEXT NOT NULL,
-                focus_time REAL NOT NULL,
-                UNIQUE(date, app_name)
-            )
-        """)
-        conn.commit()
-        conn.close()
+    
